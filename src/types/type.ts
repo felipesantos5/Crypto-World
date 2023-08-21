2dbd8d6051e05f4243bc87c39fb2102a6e70c7c9
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-08-21 17:51:37 -0300

    feat: create type config to the project

---
+++ src/types/type.ts
@@ -0,0 +1,24 @@
+export interface CoinData {
+  id: string;
+  rank: string;
+  symbol: string;
+  name: string;
+  supply: string;
+  maxSupply: string;
+  marketCapUsd: string;
+  volumeUsd24Hr: string;
+  priceUsd: string;
+  changePercent24Hr: string;
+  vwap24Hr: string;
+  explorer: string;
+}
+
+export interface CryptoNoticesInterface {
+  title: string;
+  author: string;
+  description: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+  url: string;
+}
