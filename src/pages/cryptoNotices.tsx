d32faaa5e470a7b415a0314785bfe5f260a25116
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-08-28 00:15:42 -0300

    feat: create crypto notices page

---
+++ src/pages/cryptoNotices.tsx
@@ -0,0 +1,9 @@
+import { NoticesCard } from "../components/noticeCard/noticesCard";
+
+export const CryptoNotices = () => {
+  return (
+    <main className="dark:bg-slate-800 dark:text-white">
+      <NoticesCard />
+    </main>
+  );
+};
