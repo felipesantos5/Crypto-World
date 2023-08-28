864b1f485b4bfc8ea4ec69f2c29cec716c8c0d79
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-08-28 00:18:17 -0300

    feat: component price variation

---
+++ src/components/pricevariation/pricevariation.tsx
@@ -0,0 +1,15 @@
+interface ChangePercentFormatterProps {
+  children: string; // Alterado para string
+}
+
+export const PriveVariation = ({ children }: ChangePercentFormatterProps) => {
+  const formattedChangePercent = (changePercent: string) => {
+    const prefix = changePercent.charAt(0) !== "-" ? "+" : "-";
+    const absoluteValue = changePercent.replace(/[+-]/g, "");
+    const formattedValue = `${prefix}${absoluteValue.slice(0, 4)}%`;
+
+    return formattedValue;
+  };
+
+  return <p className={`font-semibold text-lg ${children.charAt(0) === "-" ? "text-red-500" : "text-green-500"}`}>{formattedChangePercent(children)}</p>;
+};
