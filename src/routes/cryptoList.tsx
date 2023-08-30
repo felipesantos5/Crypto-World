cd2f26c5abae452b625d222c44a37e02edf02832
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-08-29 22:27:25 -0300

    feat: add new page home crypto list

---
+++ src/routes/cryptoList.tsx
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import { CoinData } from "../types/type";
+import { fetchCoinData } from "../services/cryptoApiCoins";
+import coinImages from "../constants/imageData";
+import { SkeletonCardMostPopular } from "../components/skeletonLoader/skeletonMostPopular";
+import { PriveVariation } from "../components/pricevariation/pricevariation";
+import { motion } from "framer-motion";
+
+const coinIds = ["bitcoin", "ethereum", "tether", "binance-coin", "polygon", "cardano", "solana", "polkadot", "avalanche", "dogecoin", "xrp", "uniswap", "usd-coin", "chainlink", "litecoin"];
+
+export const CryptoList = () => {
+  const [coinData, setCoinData] = useState<CoinData[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchCoinData(coinIds);
+      setCoinData(data);
+      setLoading(false);
+    };
+
+    fetchData();
+
+    const interval = setInterval(() => {
+      fetchData();
+    }, 120);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <main className="dark:bg-slate-800 dark:text-white">
+      <section className="flex flex-wrap justify-center gap-10 py-10  max-w-7xl m-auto min-h-80vh overflow-x-hidden">
+        {loading
+          ? Array.from({ length: 15 }).map((_, index) => <SkeletonCardMostPopular key={index} />)
+          : coinData.map((coin) => (
+              <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ ease: "easeOut", duration: 2 }}
+                className="flex flex-col justify-between shadow-xl rounded-lg w-52 h-60 p-4 bg-white text-black"
+                key={coin.id}
+              >
+                <div className="flex justify-between mt-2 flex-wrap">
+                  <img src={`/images/${coinImages[coin.id]}`} alt={coin.id} className="w-14" />
+                  <PriveVariation children={coin.changePercent24Hr} />
+                </div>
+                <div>
+                  <h2 className="font-semibold text-xl">{coin.name}</h2>
+                  <p className="text-gray-600 text-lg font-semibold">$ {coin.priceUsd.slice(0, 7)}</p>
+                </div>
+                <div>
+                  <a href={coin.explorer} target="_blank" className="underline ">
+                    see more
+                  </a>
+                </div>
+              </motion.div>
+            ))}
+      </section>
+    </main>
+  );
+};
