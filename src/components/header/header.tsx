8a36df2b00b98af4dc12a42ed48ef2a9f2b91c7d
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-08-29 22:26:48 -0300

    fix: correting the header redirects

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,14 +4,13 @@ import { MenuHamburguer } from "./menuHamburguer/menuHamburguer";
 
 export const Header = () => {
   return (
-    <header className="bg-slate-50 dark:bg-slate-800 dark:text-white">
+    <header className="dark:bg-slate-800 dark:text-white">
       <div className="flex items-center justify-between px-6 max-w-7xl h-10vh m-auto">
         <div>
           <h2 className="text-5xl font-bold lg:text-4xl md:text-3xl">Crypto World</h2>
         </div>
-        <div className="flex items-center gap-8 font-semibold text-xl md:hidden slg:text-lg">
-          <Link to="/">Home</Link>
-          <Link to="/mostpopular">Most Popular</Link>
+        <div className="flex items-center gap-8 font-semibold text-xl sm:hidden slg:text-lg">
+          <Link to="/">CryptoList</Link>
           <Link to="/cryptonotices">Crypto Notices</Link>
           <DarkmodeButton />
         </div>
