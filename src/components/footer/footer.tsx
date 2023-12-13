f6147ee34447891ac400de4d75406db0cd5f1f05
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-12-13 11:30:17 -0300

    reafactor : formating the new date

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer className="text-center font-semibold h-full dark:bg-slate-800 dark:text-white py-5 px-2">
       <Link to="/" className="sm:text-sm">
-        Copyright © 2023. All rights are reserved
+        Copyright © {new Date().getFullYear()}. All rights are reserved
       </Link>
     </footer>
   );
