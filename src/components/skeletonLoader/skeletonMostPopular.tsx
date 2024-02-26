d42466f4aa7f0316a50aa92acd8fbacad9da119e
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2024-02-26 10:52:28 -0300

    style: changing

--- src/components/skeletonLoader/skeletonMostPopular.tsx
+++ src/components/skeletonLoader/skeletonMostPopular.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 export const SkeletonCardMostPopular = () => {
   return (
     <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ ease: "easeOut", duration: 3 }}>
-      <div className="w-52 h-60 bg-gray-300 dark:bg-zinc-900 shadow-xl rounded-lg"></div>
+      <div className="w-52 h-60 bg-gray-300 dark:bg-zinc-700 shadow-xl rounded-lg"></div>
     </motion.div>
   );
 };
